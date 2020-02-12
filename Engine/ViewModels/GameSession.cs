7113ee2e989a6ebfef1f35c7e7fdcbf999da5a10
Wil Hunt <huntwj@gmail.com>
2020-02-11 22:42:26 -0800

    Lesson 03.4: Creating the GameSession (ViewModel) Class - make Player class and fields public - add GameSession class and initialize a CurrentPlayer

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,18 @@
+﻿using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player
+            {
+                Name = "Scott",
+                Gold = 1_000_000
+            };
+        }
+    }
+}
