b6ce6f0d268bc2b54c1bd8dc1334911265c606e4
Wil Hunt <huntwj@gmail.com>
2020-02-11 22:52:16 -0800

    Lesson 03.6 – Update Player data with the PropertyChanged event - update Player class to broadcast property change events - add Add XP button to UI to test things a little

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
 ﻿using Engine.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFUI
 {
@@ -31,6 +18,10 @@ namespace WPFUI
             _gameSession = new GameSession();
 
             DataContext = _gameSession;
+        }
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperiencePoints += 10;
         }
     }
 }
