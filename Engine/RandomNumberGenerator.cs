d0e14d8fce6f9e13deb4a400d76ca22e73e83233
Wil Hunt <huntwj@gmail.com>
2020-02-12 22:43:45 -0800

    Lesson 07.1: Creating Monsters - add new items from monster drops - add MonsterFactory class - add monster images - add Monster class - add RandomNumberGenerator class

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Engine
+{
+    public static class RandomNumberGenerator
+    {
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+        // I don't like this implementation. I prefer something more like that given in the reference docs:
+        // https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.rngcryptoserviceprovider?view=netframework-4.8
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            byte[] randomNumber = new byte[1];
+
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            // We are using Max.Max, and subtracting 0.00000000001,
+            // to ensure "multiplier" will always be between 0.0 and .999999999
+            // Otherwise, it's possible for it to be "1", which causes problems in our rounding.
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.0000000001d);
+
+            int range = maximumValue - minimumValue + 1;
+
+            double randomValueInRange = Math.Floor(multiplier * range);
+
+            return (int)(minimumValue + randomValueInRange);
+        }
+
+        // Simple version, with less randomness.
+        //
+        // If you want to use this version,
+        // you can delete (or comment out) the NumberBetween function above,
+        // and rename this from SimpleNumberBetween to NumberBetween
+        private static readonly Random _simpleGenerator = new Random();
+        public static int SimpleNumberBetween(int minimumValue, int maximumValue)
+        {
+            return _simpleGenerator.Next(minimumValue, maximumValue + 1);
+        }
+    }
+}
